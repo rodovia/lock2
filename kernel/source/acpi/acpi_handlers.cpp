fe223ad9e996c26274d441c88945bc5ac628bca0
rodovia <rodoviabr102@gmail.com>
2023-07-22 14:05:42 -0300

    acpi: Added some handlers to acpica oslayer

--- kernel/source/acpi/acpi_handlers.cpp
+++ kernel/source/acpi/acpi_handlers.cpp
@@ -1,5 +1,12 @@
 /* ACPICA OS Layer impl */
 
+/* Yes, this file breaks all of Lock2's coding conventions...
+  ...Just to adhere to ACPICA ones. 
+  Try to fix it, if you are not lazy. =) */
+
+#include "arch/i386/port.h"
+#include "arch/i386/spinlock.h"
+#include "arch/i386/timer/tsc.h"
 #include "scheduler/scheduler.h"
 #include "scheduler/semaphore.h"
 #include "scheduler/thread.h"
@@ -16,7 +23,22 @@ extern "C"
 #include "acpica/acpiosxf.h"
 }
 
+[[gnu::always_inline]]
+static inline void cli()
+{
+    asm volatile("cli");
+}
+
+[[gnu::always_inline]]
+static inline void sti()
+{
+    asm volatile("sti");
+}
+
 #pragma GCC diagnostic ignored "-Wunused-parameter"
+
+extern "C"
+{
 
 ACPI_STATUS 
 AcpiOsInitialize()
@@ -140,7 +162,143 @@ AcpiOsWaitSemaphore(ACPI_SEMAPHORE Handle, UINT32 Units, UINT16 Timeout)
     sched::semaphore* sem = reinterpret_cast<sched::semaphore*>(Handle);
     if (Timeout == 0)
     {
+        if (!sem->IsFree())
+        {
+            return AE_TIME;
+        }
+
         sem->Wait();
+        return AE_OK;
     }
-    return AE_TIME;
+
+    if (Timeout == (uint16_t)-1)
+    {
+        sem->Wait();
+        return AE_OK;
+    }
+
+    int result = sem->Wait(Timeout);
+    return result == sched::kSemaphoreErrorOk ? AE_OK : AE_TIME;
+}
+
+ACPI_STATUS AcpiOsSignalSemaphore(ACPI_SEMAPHORE Handle, UINT32 Units)
+{
+    auto sem = reinterpret_cast<sched::semaphore*>(Handle);
+    for (uint32_t i = 0; i < Units; i++)
+    {
+        sem->Notify();
+    }
+
+    return AE_OK;
+}
+
+ACPI_STATUS AcpiOsCreateLock(ACPI_SPINLOCK* spl)
+{
+    if (spl == nullptr)
+    {
+        return AE_BAD_PARAMETER;
+    }
+
+    (*spl) = new spinlock;
+    return AE_OK;
+}
+
+void AcpiOsDeleteLock(ACPI_SPINLOCK spl)
+{
+    delete (spinlock*)spl;
+}
+
+ACPI_CPU_FLAGS AcpiOsAcquireLock(void* Handle)
+{
+    SlLockAcquire((spinlock*)Handle);
+    cli();
+    return 0;
+}
+
+void AcpiOsReleaseLock(void* Handle, ACPI_CPU_FLAGS)
+{
+    SlLockRelease((spinlock*)Handle);
+    sti();
+}
+
+ACPI_STATUS AcpiOsSignal(UINT32 Function, void* Info)
+{
+    switch (Function)
+    {
+    case ACPI_SIGNAL_FATAL:
+    {
+        auto fat = reinterpret_cast<ACPI_SIGNAL_FATAL_INFO*>(Info);
+        Error("FATAL opcode inside ACPI! Type %i Code 0x%p Argument 0x%p\n",
+                fat->Type, fat->Code, fat->Argument);
+        break;
+    }
+    case ACPI_SIGNAL_BREAKPOINT:
+    {
+        Info("ACPI: Breakpoint hit! But Lock2 does not support AML debugging.\n");
+        break;
+    }
+    }
+
+    return AE_OK;
+}
+
+UINT64 AcpiOsGetTimer()
+{
+    return TscRead();
+}
+
+ACPI_STATUS AcpiOsReadPort(ACPI_IO_ADDRESS Address,
+                            UINT32* Value,
+                            UINT32 Width)
+{
+    if (Width == 8)
+    {
+        port8 p = Address;
+        (*Value) = (uint8_t)*p;
+        return AE_OK;
+    }
+    else if (Width == 16)
+    {
+        port16 p = Address;
+        (*Value) = (uint16_t)*p;
+        return AE_OK;
+    }
+    else if (Width == 32)
+    {
+        port32 p = Address;
+        (*Value) = (uint32_t)*p;
+        return AE_OK;
+    }
+
+    return AE_OK; /* Unreachable */
+}
+
+ACPI_STATUS AcpiOsWritePort(ACPI_IO_ADDRESS Address,
+                            UINT32 Value,
+                            UINT32 Width)
+{
+    if (Width == 8)
+    {
+        port8 p = Address;
+        (*p) = Value;
+        return AE_OK;
+    }
+    else if (Width == 16)
+    {
+        port16 p = Address;
+        (*p) = Value;
+        return AE_OK;
+    }
+    else if (Width == 32)
+    {
+        port32 p = Address;
+        (*p) = Value;
+        return AE_OK;
+    }
+
+    return AE_OK; /* Unreachable */
+}
+
+
+
 }
